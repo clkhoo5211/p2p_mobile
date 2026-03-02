269191c9197adcd6adee51ad5ae49b7439533606
clkhoo5211 <clkhoo5211@gmail.com>
2026-03-02 15:45:07 +0800

    02a7e389aa43b20ed7676fcfc8b5bab0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "faa9aab462d5b086a6bdb6136ca336a2",
+const RESOURCES = {"flutter_bootstrap.js": "bea49cc580f1ac99c1593b58b4e3d96e",
 "version.json": "a90d71741e23038d306f9bcda4b714ea",
 "index.html": "8a56c1dafb275be364ef93fd3eafbea5",
 "/": "8a56c1dafb275be364ef93fd3eafbea5",
-"main.dart.js": "d6ba34f36dff7e8a3635ce9d79c7abb8",
+"main.dart.js": "02a7e389aa43b20ed7676fcfc8b5bab0",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
